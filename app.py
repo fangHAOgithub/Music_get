52fd88012367a2e914829b3cdbad31eda9b38657
fangHAO <15778727884@sina.cn>
2021-12-12 16:18:11 +0800

    去除调试信息，格式化代码

--- app.py
+++ app.py
@@ -3,24 +3,23 @@ from Netease_music import Music
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/home')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/home',  methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
         return render_template('home.html')
 
     music_name = request.form.get("musicname", None)
-    print(music_name)
     if not music_name:
         # 空
         return redirect('home.html')
     music = Music()
     response = music.search_music(music_name)
-    # print(response)
     return render_template('home.html', res=response)
 
 
